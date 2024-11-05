401e7cd88b3db225abdeef963af088e49beb02f5
calderac95 <ccaldera951@gmail.com>
2024-11-04 23:16:27 -0600

    [Carlos] Agregando cambios fase 1

--- python_projects/hello_world.py
+++ python_projects/hello_world.py
@@ -1,3 +1,7 @@
 print ("Hello World")
+<<<<<<< Updated upstream
 print ("This is just a test file for this new repository, Holi")
 #No borres esto
+=======
+print ("This is just a test file for this new repository")
+>>>>>>> Stashed changes
