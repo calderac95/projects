3ef1a9869895537a1e78bcbea79b97fd8342d00c
calderac95 <ccaldera951@gmail.com>
2024-11-04 23:23:19 -0600

    Update hello_world.py

--- python_projects/hello_world.py
+++ python_projects/hello_world.py
@@ -1,7 +1,2 @@
 print ("Hello World")
-<<<<<<< Updated upstream
 print ("This is just a test file for this new repository, Holi")
-#No borres esto
-=======
-print ("This is just a test file for this new repository")
->>>>>>> Stashed changes
